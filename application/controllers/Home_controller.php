3396e776116686ea65312700511021c232f88cb5
syaifuladala <adalasyaiful@gmail.com>
2019-06-02 00:03:45 +0700

    tambah

---
+++ application/controllers/Home_controller.php
@@ -0,0 +1,13 @@
+<?php
+
+class Home_controller extends CI_Controller{
+    
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function index(){
+        $this->load->view('index');
+    }
+    
+}
---
+++ application/views/partial/header.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+</head>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('desain/frontend/css/bootstrap.min.css') ?>">
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('desain/frontend/css/animate.css') ?>">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('desain/frontend/css/owl.carousel.min.css') ?>"> 
+    <!-- themify CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('desain/frontend/css/themify-icons.css') ?>"> 
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('desain/frontend/css/flaticon.css') ?>"> 
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('desain/frontend/css/magnific-popup.css') ?>"> 
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('desain/frontend/css/slick.css') ?>">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('desain/frontend/css/style.css') ?>"> 
+
+<body>
+    <!--::header part start::-->
+    <header class="main_menu home_menu">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="index.html"> <img src="<?php echo base_url('desain/frontend/img/logo.png') ?>" alt="logo"> </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse main-menu-item justify-content-end"
+                            id="navbarSupportedContent">
+                            <ul class="navbar-nav align-items-center">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="<?php echo base_url('Home_controler') ?>">Beranda</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="about.html">Try Out</a>
+                                </li>
+                                <li class="d-none d-lg-block">
+                                    <a class="btn_1" href="#">Daftar/Masuk</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header part end-->
+
+
+</body>
+
+</html>
